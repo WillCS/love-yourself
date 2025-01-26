27718b3a7394cc18efcf761a3c5ae2a2475d135b
Will Stibbards <will@willcs.dev>
2025-01-27 01:13:53 +1000

    a little more work on plist updates before I clock off

--- macos.lua
+++ macos.lua
@@ -2,6 +2,21 @@ local constants = require('constants')
 local utils = require('utils')
 local unix_utils = require('unix')
 
+local function update_plist(location)
+  local plist_file = io.open(location, "r");
+
+  if plist_file == nil then
+    error('Could not open Info.plist')
+  end
+
+  local plist_lines = {}
+  for line in plist_file:lines() do
+    print(line)
+    table.insert (plist_lines, line);
+  end
+
+  error('Not implemented')
+end
 
 local function build(file_name, love_file_path, love_location)
   -- copy the love app to the temp dir
@@ -15,19 +30,7 @@ local function build(file_name, love_file_path, love_location)
 
   -- update Info.plist
   local plist_location = utils.path(constants.TEMP_DIR_NAME, file_name .. '.app', 'Contents', 'Info.plist')
-  local plist_file = io.open(plist_location, "r");
-
-  if plist_file == nil then
-    error('Could not open Info.plist')
-  end
-
-  local plist_lines = {}
-  for line in plist_file:lines() do
-    print(line)
-    table.insert (plist_lines, line);
-  end
-
-  error('Not implemented')
+  update_plist(plist_location)
 
   -- move the finished app file to the desired output location
   error('Not implemented')
